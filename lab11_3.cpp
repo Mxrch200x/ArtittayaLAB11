d68520b7139399cb48bd02232509754d945c7a52
Mxrch200x <gaerdbigsis@gmail.com>
2023-01-03 17:15:47 +0700

    complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cstdlib>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main(){
+    double N = 0;
+    double sum = 0, Mean, Standarddeviation, MM;
+    ifstream source;
+    source.open("score.txt");
+
+    string textline;
+    while (getline(source, textline))
+    {
+        sum += stod(textline);
+        MM += pow(stod(textline),2);
+        N++;
+    }
+    Mean = sum / N;
+    Standarddeviation = sqrt(((1/N)*MM) - pow(Mean,2));
+
+    cout << "Number of data = "<< N << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< Mean << endl;
+    cout << "Standard deviation = "<< Standarddeviation;
+    return 0;
+}
